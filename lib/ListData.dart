9f86498c29a9a2865bfe23113097d01ca592133c
Zaifi <hk7928042@gmail.com>
2022-07-13 22:20:42 +0500

    Custom Class-> ListData using Constructor

--- lib/ListData.dart
+++ lib/ListData.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:nft/customClass.dart';
 
 class MyData extends StatefulWidget {
   const MyData({Key? key}) : super(key: key);
@@ -10,13 +11,24 @@ class MyData extends StatefulWidget {
 }
 
 class _MyDataState extends State<MyData> {
+  /*
   List<String> quotes = [
     "I can accept failure, everyone fails at something. But I can't accept not trying.",
     "I've never lost a game, I just ran out of time.",
     "Talent wins games, but teamwork and intelligence wins championships."
   ];
 
-  List<String> names = ["Huzaifa", "Kamran", "Hammad"];
+  List<String> names = ["Huzaifa", "Kamran", "Hammad"]; */
+
+  List<Quote> quotes = [
+    Quote(text: "Be yourself and people will like you.", author: "Jeff Keddy"),
+    Quote(
+        text: "Sometimes weak and wan, sometimes strong and full of light",
+        author: "Tahereh Mafi"),
+    Quote(
+        text: "Time you enjoy wasting is not wasted time.",
+        author: "Marthe Troly-Curtin")
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +41,7 @@ class _MyDataState extends State<MyData> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         // children: quotes.map((q) => Text(q)).toList(),
-        children: names.map((n) => Text(n)).toList(),
+        children: quotes.map((q) => Text('${q.text} --> ${q.author}')).toList(),
       ),
     );
   }
